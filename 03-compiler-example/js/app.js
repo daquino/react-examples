8d96dd20f6bd479ccc16e5d1d5826bf28454abbf
daquino <daniel.j.aquino@gmail.com>
2015-06-15 21:43:27 -0500

    Modify gitignore to allow js directory that contains compiled .jsx files.

---
+++ 03-compiler-example/js/app.js
@@ -0,0 +1,11 @@
+var App = React.createClass({displayName: "App",
+  render: function() {
+    return (
+      React.createElement("div", {className: "col-md-4 col-md-offset-4"},
+        React.createElement("h1", null, "Hello from app.js!")
+      )
+    );
+  }
+});
+
+React.render(React.createElement(App, null), document.getElementById('container'));
